9564bd69486956c8db81fa1da8cd650a9185f408
Kamol Saha <kamolsaha@Kamols-MacBook-Air.local>
2022-01-04 11:17:21 +0000

    page object is added

---
+++ page-objects/googleAdvancedSearch.js
@@ -0,0 +1,15 @@
+module.exports = {
+    url: 'https://www.google.co.uk/advanced_search',
+    elements: {
+        mainQueryInput:'#xX4UFf',
+        mainQuery:'Elon Musk',
+        languageDropDown:'#lr_button',
+        languageDropValue:'.goog-menuitem[value = "lang_af"]',
+        lastUpdateDropDown:'#as_qdr_button',
+        lastUpdateDropDownValue:'.goog-menuitem[value ="d"]',
+        submitButton: ".jfk-button[value='Advanced Search']"
+    },
+    commands: [{
+
+    }]
+}
